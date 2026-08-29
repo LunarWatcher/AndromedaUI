5fdea4e76e99481968127800e988cecebd740af8
Olivia <oliviawolfie@pm.me>
2026-08-30 01:57:26 +0200

    Add warning comments to the compat macros

--- tests/src/migrate/GTestCompat.hpp
+++ tests/src/migrate/GTestCompat.hpp
@@ -3,6 +3,18 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+/**
+ * Do not use: this purely exists to avoid rewriting all the GTest macros.
+ * Use catch2 directly instead
+ */
 #define EXPECT_EQ(a, b) REQUIRE(a == b)
+/**
+ * Do not use: this purely exists to avoid rewriting all the GTest macros.
+ * Use catch2 directly instead
+ */
 #define EXPECT_NEAR(a, b, delta) REQUIRE_THAT((b), Catch::Matchers::WithinAbs((a), (delta)))
+/**
+ * Do not use: this purely exists to avoid rewriting all the GTest macros.
+ * Use catch2 directly instead
+ */
 #define TEST(a, b) TEST_CASE(#a #b)
